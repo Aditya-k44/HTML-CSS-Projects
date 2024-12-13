4e677b81f9cf61bafbe73ac13a877185f2b8da69
Aditya-k44 <kulkarniaditya94@gmail.com>
2024-12-13 19:40:31 +0530

    Aligned the code

--- Light & Dark Mode/index.js
+++ Light & Dark Mode/index.js
@@ -1,12 +1,12 @@
 const themeSwitch = document.getElementById("themeSwitch");
 const innerThemeSwitch = document.getElementById("innerThemeSwitch");
 
-themeSwitch.addEventListener('change', function(){
-    if(themeSwitch.checked){
-        document.body.classList.add('dark');
-        innerThemeSwitch.checked = true;
-    }else{
-        document.body.classList.remove('dark');
-        innerThemeSwitch.checked = false;
-    }
+themeSwitch.addEventListener("change", function () {
+  if (themeSwitch.checked) {
+    document.body.classList.add("dark");
+    innerThemeSwitch.checked = true;
+  } else {
+    document.body.classList.remove("dark");
+    innerThemeSwitch.checked = false;
+  }
 });
